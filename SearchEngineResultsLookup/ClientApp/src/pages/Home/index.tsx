6a4be4db81e0df0215c97a5ea657fab4a98817b6
shishirajm <shishirajm@gmail.com>
2020-12-23 02:05:57 +1100

    Made styled components look bit better.

--- SearchEngineResultsLookup/ClientApp/src/pages/Home/index.tsx
+++ SearchEngineResultsLookup/ClientApp/src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement, useState, useEffect } from 'react';
 
-import { Container, Header} from './styles';
+import { Container, Wrapper, Section, Title, Label, Input, Select, Result} from './styles';
 
 export default function Home(): ReactElement {
 
@@ -43,50 +43,57 @@ export default function Home(): ReactElement {
 
   return (
     <Container>
-      <Header>
-        <h2>Lookup Search Engine</h2>
-        <div>
-          Keyword:
-          <input
-              value={keyWord}
-              onChange={(e): void => setKeyWord(e.target.value)}
-              type="text"
-              id="keyWord"
-              placeholder="e-settlements"
-            />
+      <Wrapper>
+        <Section>
+          <Title>Lookup Search Engine</Title>
+          <div>
+            <Label>Keyword:</Label>
+            <Input
+                value={keyWord}
+                onChange={(e): void => setKeyWord(e.target.value)}
+                type="text"
+                id="keyWord"
+                placeholder="e-settlements"
+              />
+            </div>
+            <div>
+            <Label>Url to lookup:</Label>
+              <Input
+                value={url}
+                onChange={(e): void => setUrl(e.target.value)}
+                type="text"
+                id="url"
+                placeholder="www.sympli.com.au"
+              />
           </div>
           <div>
-            Url to lookup:
-            <input
-              value={url}
-              onChange={(e): void => setUrl(e.target.value)}
-              type="text"
-              id="url"
-              placeholder="www.sympli.com.au"
-            />
-        </div>
-        <div>
-          Search Engine:
-          <select name="searchEngine" value={searchEngine} onChange={(e) => {setSearchEngine(e.target.value)}}>
-            {searchEngines.map(o => <option key={o} value={o}>{o}</option>)}
-          </select>
-        </div>
-        <div><a href="/" onClick={(e): void => handleLookup(e)}>Lookup</a></div>
-      </Header>
-      {fetched &&
-      <div>
-        <p><b>{url}</b> found in <b>{result.length}</b> places.</p>
-        <p>
-          Position of Result: {result.join(', ')}
-        </p>
-      </div>
-      } 
-      { error !== '' &&
-        <div>{error}</div>
-      }
-      {
-        loading && <div>Finding the {url} in {keyWord} search on {searchEngine}.</div>
-      }
+          <Label>Search Engine:</Label>
+            <Select name="searchEngine" value={searchEngine} onChange={(e) => {setSearchEngine(e.target.value)}}>
+              {searchEngines.map(o => <option key={o} value={o}>{o}</option>)}
+            </Select>
+          </div>
+          <div><a href="/" onClick={(e): void => handleLookup(e)}>Lookup</a></div>
+        </Section>
+      </Wrapper>
+      <Wrapper>
+        <Section>
+          <Label>Search Results:</Label>
+          {fetched &&
+          <div>
+            <Result><b>{url}</b> found in <b>{result.length}</b> places.</Result>
+            <Result>
+              Position of Result: {result.join(', ')}
+            </Result>
+          </div>
+          } 
+          { error !== '' &&
+            <Result>{error}</Result>
+          }
+          {
+            loading && <Result>Finding the {url} in {keyWord} search on {searchEngine}.</Result>
+          }
+        </Section>
+      </Wrapper>
     </Container>
   );
 }
--- SearchEngineResultsLookup/ClientApp/src/pages/Home/styles.ts
+++ SearchEngineResultsLookup/ClientApp/src/pages/Home/styles.ts
@@ -2,7 +2,32 @@ import styled, { css } from 'styled-components';
 
 export const Container = styled.div``;
 
-export const Header = styled.header`
+export const Wrapper = styled.section`
+  padding: 4em;
+  background: #707070;
+`
+
+export const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: white;
+`
+
+export const Label = styled.h2`
+  font-size: 1.2em;
+  text-align: left;
+  color: white;
+  width: 20%;
+`
+
+export const Result = styled.h3`
+  font-size: 1.1em;
+  text-align: left;
+  color: white;
+  width: 80%;
+`
+
+export const Section = styled.header`
   background-color: #909090;
   padding: 30px 40px;
   color: white;
@@ -12,38 +37,14 @@ export const Header = styled.header`
     display: flex;
     align-items: center;
 
-    > input {
-      outline: none;
-      margin: 10px;
-      border: none;
-      border-radius: 0;
-      width: 80%;
-      padding: 10px;
-      font-size: 16px;
-      border-radius: 3px;
-    }
-
-    > select {
-      font-size: 16px;
-      font-weight: 700;
-      color: #444;
-      line-height: 1.3;
-      padding: .6em 1.4em .5em .8em;
-      width: 80%;
-      max-width: 80%;
-      box-sizing: border-box;
-      margin: 0;
-      border: 1px solid #aaa;
-      border-radius: 0;
-    }
-
     > a {
+      padding: 0.5em;
+      margin: 0.5em;
       display: block;
       align-items: center;
       text-decoration: none;
       border: 0;
-      padding: 10px;
-      width: 23%;
+      width: 90%;
       background: #d9d9d9;
       color: #555;
       text-align: center;
@@ -60,4 +61,29 @@ export const Header = styled.header`
       background-color: #bbb;
     }
   }
+`
+
+export const Input = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: black;
+  background: White;
+  border: none;
+  border-radius: 3px;
+  width: 70%;
+  font-size: 16px;
+  border-radius: 3px;
+`
+
+export const Select = styled.select`
+  padding: 0.5em;
+  margin: 0.5em;
+  font-size: 16px;
+  line-height: 1.3;
+  width: 70%;
+  max-width: 80%;
+  box-sizing: border-box;
+  margin: 0;
+  border: 1px solid #aaa;
+  border-radius: 0;
 `;
